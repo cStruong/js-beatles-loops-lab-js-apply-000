35f4aca9d4b8bcfcc8f4359adc56fb7a7deacfd7
CHRISTOPHER TRUONG <christophertruongm@gmail.com>
2019-01-04 21:55:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// add solution here
+function theBeatlesPlay(arrM, arrI) {
+  var finalArr = [];
+  
+  for (var i = 0; i < arrM.length; i++) {
+    var currentM = arrM[i];
+    var currentI = arrI[i];
+    
+    finalArr.push(currentM + ' plays ' + currentI);
+  }
+  
+  return finalArr;
+}
+
+function johnLennonFacts(arrF) {
+  
+}
